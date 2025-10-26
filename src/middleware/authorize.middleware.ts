7398b241e2941303f4893c7250c57204d4f6b288
SDhakZ <sharnam34@gmail.com>
2025-10-26 22:20:33 +0700

    refactor: Type safing the role in authorize middleware

--- src/middleware/authorize.middleware.ts
+++ src/middleware/authorize.middleware.ts
@@ -1,14 +1,15 @@
 import { Response, NextFunction } from "express";
 import { AuthRequest } from "./authenticate.middleware";
 import { createError } from "../utils/errors";
+import { Role } from "@prisma/client";
 
-export function authorize(roles: string[]) {
+export function authorize(roles: Role[]) {
   return (req: AuthRequest, _res: Response, next: NextFunction) => {
     if (!req.user) {
       throw createError({ message: "Unauthorized", status: 401 });
     }
 
-    if (!roles.includes(req.user.role || "")) {
+    if (!roles.includes(req.user.role as Role)) {
       throw createError({ message: "Forbidden", status: 403 });
     }
 

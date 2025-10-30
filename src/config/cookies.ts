e905f22acf1e5665452e826fa4477eff6c5bcae7
SDhakZ <sharnam34@gmail.com>
2025-10-30 18:01:29 +0700

    refactor: Removal of comment

--- src/config/cookies.ts
+++ src/config/cookies.ts
@@ -11,6 +11,6 @@ export const CLEAR_AUTH_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
-  path: "/", // must match above
+  path: "/",
   domain: "localhost",
 } as const;
